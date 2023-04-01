0625e2d84f685751be6ba26b07c24a93bf9ddb7e
hyb <2419805602@qq.com>
2023-04-01 18:05:03 +0800

    office ubuntu16 ideaIU

--- src/main/java/com/itheima/web/AddServlet.java
+++ src/main/java/com/itheima/web/AddServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -18,9 +19,21 @@ public class AddServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 1. 接收数据, request.getParameter不能接收json的数据
-        String brandName = req.getParameter("brandName");
-        System.out.println(brandName);
+//        String brandName = req.getParameter("brandName");
+//        System.out.println(brandName);
+        // 获取请求体数据
+        BufferedReader br = req.getReader();
+        String params = br.readLine();
 
+        // 将JSON字符串转为java对象
+        Brand brand = JSON.parseObject(params, Brand.class);
+//        System.out.println(brand);
+
+        // 调用service方法添加
+        service.add(brand);
+
+        // 3. 响应成功标识
+        resp.getWriter().write("success");
 
     }
 
