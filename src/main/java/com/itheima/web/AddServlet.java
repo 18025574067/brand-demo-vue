41e4db32bcac7da9c30ec25c4ca3ed5e63a8c70c
hyb <2419805602@qq.com>
2023-04-01 16:56:57 +0800

    office ubuntu16 ideaIU

---
+++ src/main/java/com/itheima/web/AddServlet.java
@@ -0,0 +1,36 @@
+package com.itheima.web;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/addServlet")
+public class AddServlet extends HttpServlet {
+    private BrandService service = new BrandService();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 调用service方法完成查询
+        List<Brand> brands = service.selectAll();
+
+        // 将集合转换为JSON数据 -- 序列化
+        String jsonString = JSON.toJSONString(brands);
+
+        // 3. 响应数据
+        resp.setContentType("text/json;charset=utf-8");
+        resp.getWriter().write(jsonString);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
